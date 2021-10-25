4c2a0d5a208b32b95b2f7074d8fd3db480bc92a4
Ankan Sharma <36896102+Ankan1998@users.noreply.github.com>
2021-10-25 20:31:44 +0530

    convolution layer torch modified to same padding as in keras

---
+++ helper/conv_same_padding.py
@@ -0,0 +1,14 @@
+# Credit goes to sumanmichael/conv_2d_with_same_padding.py
+
+import torch.nn as nn
+from functools import reduce
+from operator import __add__
+
+class Conv2dSamePadding(nn.Conv2d):
+    def __init__(self,*args,**kwargs):
+        super(Conv2dSamePadding, self).__init__(*args, **kwargs)
+        self.zero_pad_2d = nn.ZeroPad2d(reduce(__add__,
+            [(k // 2 + (k - 2 * (k // 2)) - 1, k // 2) for k in self.kernel_size[::-1]]))
+
+    def forward(self, input):
+        return  self._conv_forward(self.zero_pad_2d(input), self.weight, self.bias)
