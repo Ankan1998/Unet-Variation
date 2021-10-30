19143e7d8de152017dfe6b6ea6946d6754a7069a
Ankan1998 <ankanroni3@gmail.com>
2021-10-30 16:26:38 +0530

    minor update in residual down conv block

--- Unet_Module/residual_down_conv_block.py
+++ Unet_Module/residual_down_conv_block.py
@@ -4,11 +4,11 @@ from helper.conv_same_padding import Conv2dSamePadding
 
 
 class DownResBlock(nn.Module):
-    def __init__(self, input_channel, output_channel):
+    def __init__(self, input_channel, output_channel,strides=(1,1)):
         super(DownResBlock, self).__init__()
-        self.conv1 = Conv2dSamePadding(input_channel, output_channel, (3, 3),(2,2))
-        self.conv2 = Conv2dSamePadding(output_channel, output_channel, (3, 3))
-        self.shortcut = Conv2dSamePadding(input_channel, output_channel, (1, 1),(2,2))
+        self.conv1 = Conv2dSamePadding(input_channel, output_channel, (3, 3),stride=strides)
+        self.conv2 = Conv2dSamePadding(output_channel, output_channel, (3, 3),stride=(1,1))
+        self.shortcut = Conv2dSamePadding(input_channel, output_channel, (1, 1),stride=strides)
         self.batchnorm1 = nn.BatchNorm2d(input_channel)
         self.batchnorm2 = nn.BatchNorm2d(output_channel)
         self.relu = nn.ReLU()
@@ -26,7 +26,7 @@ class DownResBlock(nn.Module):
 
 
 if __name__ == "__main__":
-    down_res = DownResBlock(64, 128)
+    down_res = DownResBlock(64, 128,(2,2))
     inp = torch.rand(4, 64, 256, 256)
     out = down_res(inp)
     print(out.size())
